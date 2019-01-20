f41a5e4975cd2c1a2ae68f557664aeec2de33704
Vladimir-Kuchinskiy <kuchinskiy.wladimir@gmail.com>
2019-01-20 19:50:55 +0200

    Fix sign up and null false for shouts

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,9 @@
 class UsersController < Clearance::UsersController
+  def new
+    @user = User.new
+    render template: 'users/new'
+  end
+
   def show
     @user = User.find(params[:id])
     @shouts = @user.shouts
